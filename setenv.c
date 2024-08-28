b7dd21064109314563dbf768c043dfb9faace051
Anthony <peakybeetle@gmail.com>
2024-08-28 04:16:52 -0700

    setenv.c

---
+++ setenv.c
@@ -0,0 +1,97 @@
+#include "shell.h"
+
+/**
+ * get_env - Retrieves a copy of the environment
+ * variables as a string array.
+ * @info: A pointer to the info_t structure.
+ * Return: A pointer to the string array containing the
+ * environment variables, or NULL on failure.
+ */
+
+char **get_env(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
+
+/**
+ * _unsetenv - Removes an environment variable.
+ * @info: A pointer to the info_t structure.
+ * @var: The name of the environment variable to remove.
+ *
+ * Return: 1 if the environment variable was removed, 0 otherwise.
+ */
+
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t l = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), l);
+			l = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		l++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - Initializes a new environment variable
+ * or modifies an existing one.
+ * @info: A pointer to the info_t structure.
+ * @var: The name of the environment variable.
+ * @value: The value to assign to the environment variable.
+ *
+ *  Return: 0 on success, 1 on failure.
+ */
+
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
